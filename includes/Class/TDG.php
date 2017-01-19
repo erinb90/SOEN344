bb97f28b683847a0dfcf5c93d9614e13797b41a9
Dimitri Topaloglou <dtopaloglou@gmail.com>
2017-01-18 20:14:51 -0500

    #3 Refactor -Added TDG interface

---
+++ includes/Class/TDG.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: dimitri
+ * Date: 2017-01-18
+ * Time: 7:55 PM
+ */
+interface TDG
+{
+
+    public function getPk();
+    public function getTable();
+
+}
