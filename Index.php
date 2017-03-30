8e6db021dcad4f72f3f338d7f34ce91cc3d24210
Jeremy Melnyk <jeremyjm91@gmail.com>
2017-03-30 15:07:28 -0400

    Reverted jquery to 2.2.4

--- Index.php
+++ Index.php
@@ -100,7 +100,7 @@ $Test->test();
 <!-- vendor scripts & fall backs -->
 
 <!-- jQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script> window.$ || window.jQuery || document.write('<script src="node_modules/jquery/dist/jquery.min.js">\x3C/script>')</script>
 
 <!-- jQuery Cookie-->
